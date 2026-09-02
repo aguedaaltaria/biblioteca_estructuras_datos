acfca982ef469b02569606922dd266aad73ed805
aguedaaltaria <aguedaaltaria@gmail.com>
2026-09-02 14:09:41 -0700

    Día 1: Creación de estructura y programa principal básico

---
+++ codigo/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Mucho de este archivo sera hecho con la funcion de recordar y aprender
+
+// La función principal donde arranca nuestro programa
+int main() {
+    printf("¡Hola! Este es el inicio de la biblioteca de estructuras de datos.\n");
+    
+    // Retornar 0 significa que el programa terminó sin errores
+    return 0; 
+}
